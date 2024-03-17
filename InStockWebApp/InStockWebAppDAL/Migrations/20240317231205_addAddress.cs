add85e6a9757f2b044365270caf8bd3e346cab4e
Mohammed Essam Salem <77510501+mo884@users.noreply.github.com>
2024-03-18 01:26:34 +0200

    Add Address Migration

---
+++ InStockWebApp/InStockWebAppDAL/Migrations/20240317231205_addAddress.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InStockWebAppDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_City_CityId",
+                table: "User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Photo",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "User",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_City_CityId",
+                table: "User",
+                column: "CityId",
+                principalTable: "City",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_City_CityId",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine",
+                table: "User");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Photo",
+                table: "User",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_City_CityId",
+                table: "User",
+                column: "CityId",
+                principalTable: "City",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
