d472fb518ad5263cd079da2a0d892fbddb059728
Hala <76525534+HalaX3@users.noreply.github.com>
2024-03-18 09:17:29 +0200

    Added Migration of UnitsSold In Product Enity

---
+++ InStockWebApp/InStockWebAppDAL/Migrations/20240318071153_AddedUnitsSoldInProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InStockWebAppDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUnitsSoldInProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UnitsSold",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnitsSold",
+                table: "Product");
+        }
+    }
+}
